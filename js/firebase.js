42b52eea1509af73fe8afdba2e32b4cd982e2afe
Cody G Peterson <67984976+cgpeterson@users.noreply.github.com>
2026-06-05 12:21:14 -0600

    Fix comment syntax in firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,4 @@
-/ Import SDKs
+// Import SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
